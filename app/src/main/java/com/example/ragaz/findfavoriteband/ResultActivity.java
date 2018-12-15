25657e4b02acc95e574c9f576a44042f81fd33bf
milan <milan@example.com>
2018-12-15 19:57:36 +0100

    Res dodat

--- app/src/main/java/com/example/ragaz/findfavoriteband/ResultActivity.java
+++ app/src/main/java/com/example/ragaz/findfavoriteband/ResultActivity.java
@@ -1,8 +1,26 @@
 package com.example.ragaz.findfavoriteband;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class ResultActivity extends AppCompatActivity {
 
+    ListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        String[] data =  (String[]) getIntent().getExtras().get("data");
+
+        ListView listView = (ListView) findViewById(R.id.result_listview);
+
+        adapter = new ArrayAdapter<String>(this, R.layout.list_item, data);
+        listView.setAdapter(adapter);
+    }
 
 }
